f6dd6c1c510ce26c171074d62344dd4a2b26ac53
Christoffer Endresen <christofferend@gmail.com>
2018-01-21 15:22:43 +0100

    test

---
+++ packages/@angular/cli/plugins/webpack.ts
@@ -0,0 +1,6 @@
+// Exports the webpack plugins we use internally.
+export { BaseHrefWebpackPlugin } from '../lib/base-href-webpack/base-href-webpack-plugin';
+export { GlobCopyWebpackPlugin, GlobCopyWebpackPluginOptions } from './glob-copy-webpack-plugin';
+export { NamedLazyChunksWebpackPlugin } from './named-lazy-chunks-webpack-plugin';
+export { ScriptsWebpackPlugin, ScriptsWebpackPluginOptions } from './scripts-webpack-plugin';
+export { SuppressExtractedTextChunksWebpackPlugin } from './suppress-entry-chunks-webpack-plugin';
